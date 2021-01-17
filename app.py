68122eac9634d428eee4085af5fa140847067180
anandu-sanu <nandzan55@gmail.com>
2021-01-17 15:45:45 +0530

    updated mongo connections and api

--- app.py
+++ app.py
@@ -1,32 +1,38 @@
 from flask import Flask,request,jsonify
-from flask_pymongo import PyMongo
-from datetime import datetime
+import pymongo
+from pymongo import MongoClient
+import datetime
 
 app =Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/blog"
+# connection config to pymongo
 
-mongo = PyMongo(app)
+client = MongoClient()
+client = MongoClient('localhost', 27017)
+db = client['blogsite']
+collection = db['posts']
+
+
 
 @app.route('/upload/post',methods=["POST"])
 def create_article():
-    # payload = request.json()
-
-    result = mongo.db.articles.insert_one({
-        "title":request.json["title"],
-        "content":request.json["content"],
-        "author":request.json["author"],
-        "created_at":datetime.now()
-    })
+    post = {    
+                "author":request.json["author"],
+                "title": request.json["title"],
+                "content": request.json["content"],
+                "date": datetime.datetime.utcnow()
+            }
+    posts=db.posts
+    post_id=posts.insert_one(post).inserted_id
     return jsonify({"message":"Content Created Sucessfully",
                 "content_id":result._id}),201
 
-@app.route('/show/articles',methods=["GET"])
-def show_article():
-    payload = request.json()
+# @app.route('/show/articles',methods=["GET"])
+# def show_article():
+#     payload = request.json()
 
-    result = mongo.db.articles.find()
-    return jsonify(result)
+#     result = mongo.db.articles.find()
+#     return jsonify(result)
 
 
 if __name__ == '__main__':
